26886eace625019110eaeb2e0a8eb3facf788571
Shujat Ali <the.shujat@gmail.com>
2022-10-24 15:25:20 +0200

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace ContosoMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public ActionResult Welcome()
+        {
+            //return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            return View();
+        }
+        // GET: HelloWorldController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HelloWorldController/Details/5s
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HelloWorldController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HelloWorldController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HelloWorldController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HelloWorldController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Data/ContosoMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoMVC.Models;
+
+namespace ContosoMVC.Data
+{
+    public class ContosoMVCContext : DbContext
+    {
+        public ContosoMVCContext (DbContextOptions<ContosoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoMVC.Models.Movie> Movie { get; set; } = default!;
+    }
+}
